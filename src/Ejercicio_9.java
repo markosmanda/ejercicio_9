337dddd12839cf22fd64b4d7f1e818c3e15af875
marcos.españa <marcos.espaa@DESKTOP-9MH23G4>
2022-11-29 13:43:24 +0100

    ejercicios 12

---
+++ src/Ejercicio_9.java
@@ -0,0 +1,136 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author marcos.españa
+ */
+public class Ejercicio_9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        menu();
+    }
+    public static void menu (){
+        int opcion;
+        
+        Scanner entrada = new Scanner (System.in);
+        
+        String cadenas = "hola me llamo juan";
+        System.out.println("***************");
+        System.out.println("Cadena: "+cadenas);
+        System.out.println("***************");
+        do{
+        System.out.println("1. Introducir cadena: ");
+        System.out.println("2. Capitalizar la cadena: ");
+        System.out.println("3. Sustituir los espacios en blanco por_ ");
+        System.out.println("4. Salir");
+        System.out.print("Introduce la opción: ");
+        opcion=Integer.parseInt(entrada.nextLine());
+       
+          
+       
+        switch (opcion){
+            case 1:
+                devolverCadena();
+                break;
+            case 2:
+                capitalizarCadena(cadenas);
+                break;
+            case 3:
+                sustituirBlancos(cadenas);
+                break;
+            case 4:
+                System.out.println("Saliendo");
+                break;
+            default:
+                System.out.println("Error");
+                break;
+                
+        }
+       } while(opcion!=4);
+    } 
+        
+    public static void devolverCadena (){
+        String cadena="";
+        Scanner entrada = new Scanner (System.in);
+                System.out.print("Introducir nueva cadena: ");
+                cadena = entrada.nextLine();
+                System.out.println("La cadena es: "+cadena);
+        
+    
+    }public static void capitalizarCadena (String frase){
+ char caracter,caracterMayus;
+        
+        
+        
+        
+      
+        String resultado="";
+        String iniciales="";
+        
+        
+        //Tendríamos que depurar espacios iniciales
+        // codigo para depurar espacios o usar el trim()
+        frase=frase.trim();
+        
+        
+        //Meto el primer caracter en mayusculas
+        caracter=frase.charAt(0);
+        caracterMayus=Character.toUpperCase(caracter);
+        resultado=resultado+caracterMayus;
+        
+        
+        for (int i = 1; i < frase.length(); i++) {
+            
+            if (frase.charAt(i-1)==' '){
+                //El caracter anterior es un espacio
+                //pongo en mayusculas el caracte actual
+                caracter=frase.charAt(i);
+                caracterMayus=Character.toUpperCase(caracter);
+                resultado=resultado+caracterMayus;
+            }
+            else{
+                caracter=frase.charAt(i);
+                resultado=resultado+caracter;
+            }
+        }
+        
+        
+        
+        
+        
+        
+        //Saco las iniciales de la frase en minusculas
+        System.out.println(resultado);
+        //Meto el primer caracter
+        iniciales=iniciales+frase.charAt(0);
+        
+        for (int i = 1; i < frase.length(); i++) {
+            if (frase.charAt(i-1)==' '){
+                //El caracter anterior es un espacio
+                //añado el caracter actual
+                iniciales=iniciales+frase.charAt(i);
+            }
+            
+        }
+        
+           
+        
+    }public static void sustituirBlancos(String frase){
+        String resultado="";
+        
+           resultado= frase.replace(' ', '_');
+        
+            System.out.println(resultado);
+    }
+    
+}
